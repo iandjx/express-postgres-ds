dbb154d335d9158a85f0d3748313366f395d1df1
Ian de Jesus <ian.dejesus.md@gmail.com>
2022-04-11 08:53:45 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  host: "nba-visualization-project.ctxd45yjwfgs.us-east-2.rds.amazonaws.com",
+  password: "Rice!Team04",
+  database: "nba-db",
+  port: 5432,
+});
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (request, response) => {
+  pool.query(
+    'SELECT "players"."Player", "players"."height", "players"."weight", "players"."born", "modern_season_stats"."Year" FROM players INNER JOIN  modern_season_stats ON "modern_season_stats"."Player"="players"."Player"',
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+  // pool.query(
+  //   "SELECT * FROM modern_season_stats INNER JOIN players ON modern_season_stats.Player=players.Player ",
+  //   (error, results) => {
+  //     if (error) {
+  //       console.log(error);
+  //     }
+  //     response.status(200).json(results.rows);
+  //   }
+  // );
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
